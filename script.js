d32150ec3274782e05e19098625cae525e0b6deb
huigovnomuravei <32498097+huigovnomuravei@users.noreply.github.com>
2019-12-13 17:56:46 +0200

    twitch split test support

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ function getElementByXpath(path) {
     return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
+let buttonClasses = [
+    'tw-button tw-button--success tw-interactive',
+    'tw-transition tw-transition--duration-long tw-transition--enter-done tw-transition__slide-over-left tw-transition__slide-over-left--enter-done'
+];
+
 function claimPoints() {
-    let claimButton = getElementByXpath('//button[@class="tw-button tw-button--success tw-interactive"]');
-    if (claimButton !== null) {
-        claimButton.click();
-        console.log('Points claimed!')
-    }
+    buttonClasses.forEach(function (item, index) {
+        let claimButton = getElementByXpath(`//button[@class="${item}"]`);
+        if (claimButton !== null) {
+            claimButton.click();
+            console.log('Points claimed!');
+        }
+    });
 }
 
 window.addEventListener('load', function(){
