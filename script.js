a6bd1726c140a00ab6ed8883b001d25767dfd831
huigovnomuravei <32498097+huigovnomuravei@users.noreply.github.com>
2019-12-21 12:46:57 +0200

    wtf
    need other approach, i think

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ function getElementByXpath(path) {
 
 let buttonClasses = [
     'tw-button tw-button--success tw-interactive',
-    'tw-transition tw-transition--duration-long tw-transition--enter-done tw-transition__slide-over-left tw-transition__slide-over-left--enter-done'
+    'tw-transition tw-transition--duration-long tw-transition--enter-done tw-transition__slide-over-left tw-transition__slide-over-left--enter-done',
+    'tw-align-items-center tw-align-middle tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-border-top-left-radius-medium tw-border-top-right-radius-medium tw-core-button tw-core-button--destructive tw-inline-flex tw-interactive tw-justify-content-center tw-overflow-hidden tw-relative'
 ];
 
 function claimPoints() {
